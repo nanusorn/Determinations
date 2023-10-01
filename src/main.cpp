40fbf13310ac1f867eda08c93d90c21fa59de6e6
Paul Photpipat <pphotpipat@deem.com>
2023-10-01 14:37:51 +0700

    first commit after generate project from https://github.com/electro-smith/DaisyBlinkProject

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
-/** Daisy Blink Example
+/** 
  *
- *  This example blinks the Daisy Seed's LED
- *  once per second, as explained in the
- *  C++ Getting Started guide for the Daisy.
-*/
+ */
 #include "daisy_seed.h"
 
 using namespace daisy;
